bc24d83051e92c587919e3e6747ce94c08321cf0
Ted Goldstein <tedgoldstein@gmail.com>
2015-05-10 14:19:12 -0700

    Bumped the version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ucscmedbook:api',
   summary: 'Code shared by all MedBook Apps',
-  version: '0.1.0',
+  version: '0.2.0',
   git: 'https://github.com/UCSC-MedBook/medbook-api.git'
 });
 
