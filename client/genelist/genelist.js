67a14a05f0fa53c727360b0381c47907b82e4594
Ted Goldstein <tedgoldstein@gmail.com>
2015-05-10 17:56:14 -0700

    Now supports multiple genelist fields in a form.

--- client/genelist/genelist.js
+++ client/genelist/genelist.js
@@ -2,25 +2,17 @@
 
 AutoForm.addInputType("genelist", {
   template: "genelist",
-  valueOut: function () {
-     debugger;
-     return;
-  },
   valueConverters: {
     "object": function (val) {
-      debugger;
       return val;
     },
     "Object": function (val) {
-      debugger;
       return val;
     },
     "string": function (val) {
-      debugger;
       return val;
     },
     "stringArray": function (val) {
-      debugger;
       return val;
     },
   }
@@ -29,30 +21,35 @@ AutoForm.addInputType("genelist", {
 Template.genelist.helpers({
    'change #genelist' : function(evt, tmpl) {
        var $genelist = tmpl.find("input");
-       debugger;
    },
 });
 
-AutoForm.addHooks(null, {
-    formToDoc: function(doc)  {
-       debugger;
-       var raw = $(".genelist").select2("val");
-       doc.Mutated_Genes = raw;
+AutoForm.addHooks("CRFquickForm", {
+    formToDoc: function(doc, ss)  {
+
+       $(".genelist").each(function(i,element) {
+           var fieldName = $(element).attr("prop")
+           if (fieldName) {
+              var raw = $(element).select2("val");
+              doc[fieldName] = raw;
+           }
+       });
+
        return doc;
     },
-    docToForm: function(doc)  {
-     var data = doc.Mutated_Genes;
-     $(".genelist").select2("data", data.map(function(e) {return { id: e, text: e}}));
 
-     /*
-     $('.genelist').select2({
-          initSelection : function (element, callback) {
-            debugger
-            if (data)
-                callback( data.map(function(g) { return { id: g, text: g }}) );
-          }
-      });
-      */
+    docToForm: function(doc, ss)  {
+
+       $(".genelist").each(function(i,element) {
+           var fieldName = $(element).attr("prop")
+           if (fieldName) {
+              var data = doc[fieldName];
+              if (data == null)
+                  $(element).select2("data", null);
+              else
+                  $(element).select2("data", data.map(function(e) {return { id: e, text: e}}));
+           }
+       });
       return doc;
     }
 });
@@ -60,16 +57,15 @@ AutoForm.addHooks(null, {
 Template.genelist.rendered = function () {
      var thisTemplate = this;;
      thisTemplate.$genelist = $(this.find("input"));
+     thisTemplate.$genelist.attr("prop", thisTemplate.data.name);
 
      var absUrl = Meteor.absoluteUrl();
      var whichApp = absUrl.substring(absUrl.lastIndexOf("/", absUrl.length -2));
      var url = whichApp + "genes";
 
-     debugger;
      thisTemplate.$genelist.select2({
           initSelection : function (element, callback) {
             var data = Template.currentData().value;
-            debugger
             if (data)
                 callback( data.map(function(g) { return { id: g, text: g }}) );
           },
