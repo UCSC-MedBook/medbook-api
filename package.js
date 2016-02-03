54f422178bcce107042823a92b33ece00cb6bd35
chrisw <chrisw@soe.ucsc.edu>
2016-02-03 11:45:45 -0800

    v0.2.9 - published with "meteor publish --release 1.1.0.2"

--- package.js
+++ package.js
@@ -1,7 +1,9 @@
 Package.describe({
   name: 'medbook:api',
   summary: 'Code shared by all MedBook Apps',
-  version: '0.2.7',
+  version: '0.2.9',
+  // v0.2.9 - published with "meteor publish --release 1.1.0.2"
+  // v0.2.8 - troubleshooting
   // v0.2.7 - set meteor version in package.js
   // v 0.2.6 - genelist server and client functionality moved to its own package
   // version 0.2.5 is same as 0.2.4 except publish with:
@@ -12,14 +14,14 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@1.1.0.2');
+  api.versionsFrom("1.1.0.2");
   // api.use('templating@1.0.0');
   // api.use('blaze@2.0.0');
   // api.use('aldeed:autoform@4.0.0');
   // api.use('cfs:http-methods@0.0.27');
 
 // api.use(['natestrauser:select2@4.0.0_1', 'templating@1.0.0', 'blaze@2.0.0', 'aldeed:autoform@4.0.0', 'cfs:http-methods@0.0.27']);
-api.use(['templating@1.0.0']);
+api.use(['templating']);
 
   // api.addFiles([
     // 'server/genelist/genelist.js',
